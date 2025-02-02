47c5f0d32fa1515258107bef1af18dd8cb016539
enfow <wgm0601@gmail.com>
2025-02-02 20:12:38 +0900

    [contentlayer] remove unuse field: contentType

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -102,7 +102,6 @@ export const Blog = defineDocumentType(() => ({
   fields: {
     title: { type: 'string', required: true },
     date: { type: 'date', required: true },
-    contentType: { type: 'string'},
     tags: { type: 'list', of: { type: 'string' }, default: [] },
     lastmod: { type: 'date' },
     draft: { type: 'boolean' },
