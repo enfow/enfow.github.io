c4551e3e130526395113e320d8cbd1f93a0f8051
enfow <wgm0601@gmail.com>
2025-02-09 00:00:31 +0900

    [common] formatting

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,9 @@
 const headerNavLinks = [
-  { href: '/', title: 'Home', type: "page", header: false },
+  { href: '/', title: 'Home', type: 'page', header: false },
   { href: '/tech', title: 'Tech', type: 'category', header: true },
   { href: '/finance', title: 'Finance', type: 'category', header: true },
-  { href: '/tech/paper-review', title: 'Paper-Review', type: "sub-category", header: false },
-  { href: '/about', title: 'About', type: "page", header: true },
+  { href: '/tech/paper-review', title: 'Paper-Review', type: 'sub-category', header: false },
+  { href: '/about', title: 'About', type: 'page', header: true },
 ]
 
 export default headerNavLinks
