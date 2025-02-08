eba5e2307cc54dc4a4bba43db122b20abd9e88de
enfow <wgm0601@gmail.com>
2025-02-08 17:58:26 +0900

    [app] fix main page and some tag related errors

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -4,10 +4,10 @@ interface Props {
   text: string
 }
 
-const Tag = ({ text }: Props) => {
+const Tag = ({ category, text }: Props) => {
   return (
     <Link
-      href={`/tags/${slug(text)}`}
+      href={`/${category}/tags/${slug(text)}`}
       className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
     >
       {text.split(' ').join('-')}
