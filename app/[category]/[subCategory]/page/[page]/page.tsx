36ee5787755af0de0f228cfd48f5c8ab2bfec5fb
enfow <wgm0601@gmail.com>
2025-02-19 17:57:24 +0900

    [app] formatting

--- app/[category]/[subCategory]/page/[page]/page.tsx
+++ app/[category]/[subCategory]/page/[page]/page.tsx
@@ -9,20 +9,6 @@ const VALID_CATEGORIES = ['tech', 'daily', 'finance']
 
 export const metadata = genPageMetadata({ title: 'Blog' })
 
-export const generateStaticParams = async () => {
-  return VALID_CATEGORIES.flatMap((category) =>
-    allBlogs
-      .filter((blog) => blog.path.startsWith(category))
-      .map((blog) => {
-        const parts = blog.path.split('/')
-        return {
-          category,
-          subCategory: parts[1] || '',
-        }
-      })
-  )
-}
-
 export default async function BlogPage({
   params,
 }: {
