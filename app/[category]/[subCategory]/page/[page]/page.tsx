edaa8fddc3e78b849ead9bbf2c56232fc714e54b
enfow <wgm0601@gmail.com>
2025-02-19 17:53:09 +0900

    [app] formatting

--- app/[category]/[subCategory]/page/[page]/page.tsx
+++ app/[category]/[subCategory]/page/[page]/page.tsx
@@ -26,9 +26,9 @@ export const generateStaticParams = async () => {
 export default async function BlogPage({
   params,
 }: {
-  params: Promise<{ subCategory: string; category: string, page: string }>
+  params: Promise<{ subCategory: string; category: string; page: string }>
 }) {
-  const { subCategory, category, page} = await params
+  const { subCategory, category, page } = await params
 
   if (!VALID_CATEGORIES.includes(category)) {
     notFound()
@@ -41,7 +41,10 @@ export default async function BlogPage({
 
   const pageNumber = parseInt(page)
   const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE)
-  const initialDisplayPosts = posts.slice(POSTS_PER_PAGE * (pageNumber - 1), POSTS_PER_PAGE * pageNumber)
+  const initialDisplayPosts = posts.slice(
+    POSTS_PER_PAGE * (pageNumber - 1),
+    POSTS_PER_PAGE * pageNumber
+  )
   const pagination = {
     currentPage: pageNumber,
     totalPages: totalPages,
--- app/[category]/page/[page]/page.tsx
+++ app/[category]/page/[page]/page.tsx
@@ -15,8 +15,12 @@ export const generateStaticParams = async () => {
   }))
 }
 
-export default async function BlogPage({ params }: { params: Promise<{ category: string, page: string}> }) {
-  const { category, page} = await params
+export default async function BlogPage({
+  params,
+}: {
+  params: Promise<{ category: string; page: string }>
+}) {
+  const { category, page } = await params
 
   if (!VALID_CATEGORIES.includes(category)) {
     notFound()
@@ -29,7 +33,10 @@ export default async function BlogPage({ params }: { params: Promise<{ category:
 
   const pageNumber = parseInt(page)
   const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE)
-  const initialDisplayPosts = posts.slice(POSTS_PER_PAGE * (pageNumber - 1), POSTS_PER_PAGE * pageNumber)
+  const initialDisplayPosts = posts.slice(
+    POSTS_PER_PAGE * (pageNumber - 1),
+    POSTS_PER_PAGE * pageNumber
+  )
   const pagination = {
     currentPage: pageNumber,
     totalPages: totalPages,
