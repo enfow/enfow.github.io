5d5a0b3baba67d30ed77ed4a770140cc5f3a871e
enfow <wgm0601@gmail.com>
2025-06-25 03:11:24 +0900

    [app] add new sub category

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/tech', title: 'Tech', type: 'category', header: true },
   { href: '/finance', title: 'Finance', type: 'category', header: true },
   { href: '/tech/paper-review', title: 'Paper-Review', type: 'sub-category', header: false },
+  { href: '/tech/blog', title: 'Blog', type: 'sub-category', header: false },
   { href: '/tech/dev', title: 'Dev', type: 'sub-category', header: false },
   { href: '/about', title: 'About', type: 'page', header: true },
 ]
